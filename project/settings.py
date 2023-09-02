2aa055053f218d5162107dd65b4947af8bde5a12
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-09-02 14:25:59 +0300

    Чтение env, DEBUG

---
+++ project/settings.py
@@ -0,0 +1,49 @@
+import os
+from os.path import dirname, join
+from environs import Env
+
+
+env = Env()
+env.read_env() 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASS"),
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = 'REPLACE_ME'
+
+DEBUG = env.bool('DEBUG', False)
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
